8a39e75e457053ad9a7819e4f2afa31de096e4bb
Dmitry Sukhikh <coddeys@gmail.com>
2015-11-04 16:21:01 -0200

    Try to make seaport follow elm-architecture

---
+++ Seaport1.elm
@@ -0,0 +1,125 @@
+module Seaport where
+
+import Char
+import Html exposing (..)
+import Html.Attributes as Attr exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Json exposing ((:=))
+import String
+import Task exposing (..)
+
+import StartApp.Simple exposing (start)
+
+-- MODEL
+
+type alias Model =
+  { seaport : Seaport,
+    queryUrl : String
+  }
+
+type alias Seaport =
+  { code: String,
+    name: String,
+    country: String
+  }
+
+emptySeaport  =
+  { code = "",
+    name = "",
+    country = ""
+  }
+
+init : String -> (Model, Result String (List Seaport))
+init emptySeaport =
+  ( Model emptySeaport "http://seaports.herokuapp.com/seaports.json?q=",
+    
+
+-- UPDATE
+
+
+
+--  View
+
+seaportStr : Seaport -> String
+seaportStr seaport =
+  String.concat [seaport.code,    ", ",
+                 seaport.name,    ", ",
+                 seaport.country, ", "
+                ]
+
+seaportList : List Seaport -> List Html
+seaportList seaports =
+  List.map
+    (\seaport -> li
+      [ ]
+      [ text (seaportStr seaport) ]) seaports
+
+
+view : String -> Result String (List Seaport) -> Html
+view message result=
+  let header =
+        h1 [ ] [ text "Seaports" ]
+      field =
+        div [ ]
+          [ input [
+             placeholder "Code of seaport",
+             on "input" targetValue (Signal.message query.address) ]
+             [ ],
+            hr [ ] [ ]                  
+          ]
+      message =
+        case result of
+          Err msg ->
+            [ div [  ] [ text msg ] ]
+
+          Ok seaports ->
+            [ ul [ ] (seaportList seaports) ]
+          
+  in
+    div [ ] (header :: field :: message)
+o
+
+-- WIRING
+
+main : Signal Html
+main =
+  Signal.map2 view query.signal results.signal
+
+
+query : Signal.Mailbox String
+query =
+  Signal.mailbox ""
+
+
+results : Signal.Mailbox (Result String (List Seaport))
+results =
+  Signal.mailbox (Err "Waiting")
+
+
+port requests : Signal (Task x ())
+port requests =
+  Signal.map lookupSeaport query.signal
+    |> Signal.map (\task -> Task.toResult task `andThen` Signal.send results.address)
+    
+lookupSeaport : String -> Task String (List Seaport)
+lookupSeaport inquiry =
+  let
+    toUrl =
+      if String.length inquiry >= 1
+        then succeed ("http://seaports.herokuapp.com/seaports.json?q=" ++ inquiry)
+        else fail "Please input some character"
+  in
+    toUrl `andThen` (mapError (always "Not found :(") << Http.get places)
+
+
+places : Json.Decoder (List Seaport)
+places =
+  let place =
+        Json.object3 Seaport
+          ("code" := Json.string)
+          ("name" := Json.string)
+          ("country" := Json.string)          
+  in
+      "seaports" := Json.list place
+
